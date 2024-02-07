55362c6e32acc6584c22f384d2ee9852ca8dd8e8
Maurius Hill <146677371+mauriushill@users.noreply.github.com>
2024-02-07 22:40:29 +0000

    everything works. no errors. accurate results.

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -1,17 +1,48 @@
 class MathController < ApplicationController 
-  def home
-    render({ :template => "math_template/home" })
-  end
-
   def square
     render({ :template => "math_template/square" })
   end
 
   def square_results
+    @number = params.fetch("user_square")
+    @square = @number.to_f ** 2
     render({ :template => "math_template/square_result"})
   end
 
   def root
     render({ :template => "math_template/square_root"})
+  end
+
+  def root_results
+    @number = params.fetch("user_root")
+    @root = @number.to_f ** 0.5
+    render ({ :template => "math_template/root_results"})
+  end
+
+  def payment  
+    render ({ :template => "math_template/payment"})
+  end 
+
+  def payment_results
+    @principal = params.fetch("user_pv").to_f
+    @apr = params.fetch("user_apr").to_f
+    r = @apr / 12.0 / 100.0
+    @years = params.fetch("user_years").to_i
+    n = @years * 12.0
+    numerator = r * @principal
+    denominator = 1.0 - ((1.0 + r) ** -n)
+    @payment = numerator / denominator
+    render ({ :template => "math_template/payment_result"})
+  end
+  
+  def random
+    render ({ :template => "math_template/random"})
+  end
+
+  def random_results
+    @min = params.fetch("user_min").to_f
+    @max = params.fetch("user_max").to_f
+    @random = rand(@min..@max).to_f
+    render ({ :template=> "math_template/random_results"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "math", :action => "home" })
+  get("/", { :controller => "math", :action => "square" })
 
   get("/square/new", { :controller => "math", :action => "square" })
 
   get("/square/results", { :controller => "math", :action => "square_results" })
 
-  get("/square_root/new", { :controller => "math", :action => "root"})
+  get("/square_root/new", { :controller => "math", :action => "root" })
+
+  get("/square_root/results", { :controller => "math", :action => "root_results"})
+
+  get("/payment/new", { :controller => "math", :action => "payment"})
+
+  get("/payment/results", { :controller => "math", :action => "payment_results"})
+
+  get("/random/new", { :controller => "math", :action => "random"})
+
+  get("/random/results", { :controller => "math", :action => "random_results"})
 end
